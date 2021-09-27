608c6e1cf6929432a22d126913c129a5ba913def
siroccomax <siroccomax@gmail.com>
2021-09-27 09:45:31 +0500

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Makarov_Maxim on 27.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
